390f6dc0e68ee9d6f715b8ad5b032ae3ab49467e
teheidoma <novakovskii2002@gmail.com>
2024-04-18 13:26:49 +0300

    init

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ghcr.io/teheidoma/spring-boot-image-build-action:0.0.1
+FROM ghcr.io/teheidoma/spring-boot-image-build-action-jdk17:0.0.2
 
 COPY entrypoint.py /entrypoint.py
 
--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,8 @@
+import json
 import re
 import subprocess
 import os
-
+import tabulate
 
 def run_and_print_output(arg):
     if type(arg) == type([]):
@@ -13,15 +14,28 @@ def run_and_print_output(arg):
         exit(p.returncode)
 
 
+def push_image(image, tag):
+    if include_commit_sha.lower() == 'true':
+        original_image = f'{image}:{tag}'
+        modified_image = f'{image}:{tag}-{github_sha}'
+        tag = f'{tag}-{github_sha}'
+        run_and_print_output(['docker', 'tag', original_image, modified_image])
+    run_and_print_output(["docker", "push", f"{image}:{tag}"])
+
+
 sdkman_dir = "/root/.sdkman"
 sdkman_init_script = "/root/.sdkman/bin/sdkman-init.sh"
 
 github_output_file = os.environ.get("GITHUB_OUTPUT")
+github_step_summary_file = os.environ.get("GITHUB_STEP_SUMMARY")
 registry_username = os.environ.get("INPUT_REGISTRY_USERNAME")
 registry_password = os.environ.get("INPUT_REGISTRY_PASSWORD")
 registry_hostname = os.environ.get("INPUT_REGISTRY_HOSTNAME")
 include_commit_sha = os.environ.get("INPUT_INCLUDE_COMMIT_SHA")
 github_sha = os.environ.get("GITHUB_SHA")[:10]
+
+if registry_username and registry_password:
+    run_and_print_output(["docker", "login", "-u", registry_username, "-p", registry_password, registry_hostname])
 
 subprocess.run(["chmod", "+x", "./gradlew"])
 process = subprocess.run(["./gradlew", "bootBuildImage"],
@@ -31,25 +45,17 @@ process = subprocess.run(["./gradlew", "bootBuildImage"],
 output = process.stdout
 print(output)
 
-pattern = r'^Successfully built image \'(.+):(.+)\'$'
-match = re.search(pattern, output, re.MULTILINE)
-if match:
-    image_name = match.group(1)
-    image_tag = match.group(2)
-else:
+reg = re.compile(r"^Successfully built image \'(.+):(.+)\'$", re.MULTILINE)
+matches = [m.groups() for m in reg.finditer(output)]
+if len(matches) < 1:
     exit(1)
 
-if registry_username and registry_password:
-    run_and_print_output(["docker", "login", "-u", registry_username, "-p", registry_password, registry_hostname])
-
-if include_commit_sha.lower() == 'true':
-    original_image = f'{image_name}:{image_tag}'
-    modified_image = f'{image_name}:{image_tag}-{github_sha}'
-    run_and_print_output(['docker', 'tag', original_image, modified_image])
-    image_tag = f'{image_tag}-{github_sha}'
+for match in matches:
+    push_image(match[0], match[1])
 
 with open(github_output_file, "a") as output_file:
-    output_file.write(f"IMAGE_NAME={image_name}\n")
-    output_file.write(f"IMAGE_TAG={image_tag}\n")
-
-run_and_print_output(["docker", "push", f"{image_name}:{image_tag}"])
+    output_file.write(f"IMAGE_NAME={json.dumps(list(map(lambda x: x[0], matches)))}\n")
+    output_file.write(f"IMAGE_TAG={matches[0][1]}\n")
+with open(github_step_summary_file, "a") as output_file:
+    markdown = tabulate.tabulate(matches, tablefmt='pipe', numalign='left', headers=['image name', 'image tag'])
+    output_file.write(markdown)
